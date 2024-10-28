8146284b789cd8d7ebbf12fd3189cf5ae41f8677
Jon Goodger <jonno.is@gmail.com>
2024-10-28 12:20:41 +0000

    Fix main branch name

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "Virakal/ScoopBucket:main"
+    [String]$upstream = "Virakal/ScoopBucket:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
